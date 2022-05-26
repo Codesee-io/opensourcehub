da32b288dab02dd05577a8a7b9eed77412fc0087
Maxime Preaux <hello@maxime.dev>
2022-05-26 15:08:41 -0700

    Allow users to log out manually

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,6 +1,12 @@
-import { redirect } from "@remix-run/node";
+import { json, LoaderFunction, redirect } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
-import { destroySession, getSession } from "~/session.server";
+import { useSubmit } from "@remix-run/react";
+import { FC, useEffect } from "react";
+import {
+  destroySession,
+  getCurrentUserOrRedirect,
+  getSession,
+} from "~/session.server";
 
 export const action: ActionFunction = async ({ request }) => {
   let session = await getSession(request.headers.get("Cookie"));
@@ -12,7 +18,24 @@ export const action: ActionFunction = async ({ request }) => {
   });
 };
 
-export const loader = () => {
-  // Redirect to the home page
-  return redirect("/");
+export const loader: LoaderFunction = async ({ request }) => {
+  await getCurrentUserOrRedirect(request);
+
+  return json({});
 };
+
+const LogOut: FC = () => {
+  const submit = useSubmit();
+
+  useEffect(() => {
+    submit(null, { method: "post", action: "/logout" });
+  }, [submit]);
+
+  return (
+    <div className="max-w-2xl mx-auto bg-white rounded-lg my-8 px-6 py-12 border border-light-border text-center">
+      <p className="text-light-type">Logging you out 👋</p>
+    </div>
+  );
+};
+
+export default LogOut;
