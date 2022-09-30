4327fba80c61075a4df824e2b29c4eac6a26b745
Maxime Preaux (they/them) <deammer@users.noreply.github.com>
2022-09-30 11:49:42 -0700

    Add a confirmation before submitting a project (#121)

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
-import cx from "classnames";
 import { Project } from "~/types";
 import ProjectTemplate from "./ProjectTemplate";
 import Button from "./Button";
+import ModalWrapper from "./ModalWrapper";
 
 type Props = {
   isOpen: boolean;
@@ -18,11 +18,7 @@ type Props = {
  */
 const ProjectPreview: FC<Props> = ({ isOpen, project, closePreview }) => {
   return (
-    <div
-      className={cx("fixed inset-0 p-2 md:p-6 z-50 bg-black-700/75", {
-        hidden: !isOpen,
-      })}
-    >
+    <ModalWrapper isOpen={isOpen}>
       <div className="max-w-6xl mx-auto shadow-lg bg-black-30 rounded-lg border-t-4 border-light-interactive">
         <div className="bg-light-interactive px-4 pb-4 pt-3 flex justify-between items-center">
           <h2 className="font-semibold text-xl text-white text-center">
@@ -34,7 +30,7 @@ const ProjectPreview: FC<Props> = ({ isOpen, project, closePreview }) => {
           {project && <ProjectTemplate project={project} />}
         </div>
       </div>
-    </div>
+    </ModalWrapper>
   );
 };
 
