12861f84e19f10dd16af690c1301635a97eb21b4
Maxime Preaux <hello@maxime.dev>
2022-10-04 17:11:36 -0700

    Removed the tag color fields

--- app/data/roles.ts
+++ app/data/roles.ts
@@ -1,76 +1,61 @@
 import { Tag } from "~/types";
-import tagColors from "./tagColors";
 
 const roles: Tag[] = [
   {
     label: "Developers",
     id: "developers",
-    color: tagColors.brown,
   },
   {
     label: "Backend Developers",
     id: "backend_developers",
-    color: tagColors.cyan,
   },
   {
     label: "Maintainers",
     id: "maintainers",
-    color: tagColors.goldYellow,
   },
   {
     label: "Frontend Developers",
     id: "frontend_developers",
-    color: tagColors.grassGreen,
   },
   {
     label: "DevOps",
     id: "devops",
-    color: tagColors.hotPink,
   },
   {
     label: "Technical Writers",
     id: "technical_writers",
-    color: tagColors.hunterGreen,
   },
   {
     label: "UX",
     id: "ux",
-    color: tagColors.lavender,
   },
   {
     label: "Designers",
     id: "designers",
-    color: tagColors.oceanBlue,
   },
   {
     label: "Testers",
     id: "testers",
-    color: tagColors.ochre,
   },
   {
     label: "Project Owners",
     id: "project_owners",
-    color: tagColors.purple,
   },
   {
     label: "Code Reviewers",
     id: "reviewers",
-    color: tagColors.rose,
   },
   {
     label: "Mentors",
     id: "mentors",
-    color: tagColors.rust,
   },
   {
     label: "Researchers",
     id: "researchers",
-    color: tagColors.scarlet,
   },
   {
     label: "Issue Triage",
     id: "issue_triage",
-    color: tagColors.seaGreen,
   },
 ];
 
--- app/data/subjects.ts
+++ app/data/subjects.ts
@@ -1,156 +1,126 @@
 import { Tag } from "~/types";
-import tagColors from "./tagColors";
 
+// 30
 const subjects: Tag[] = [
+  {
+    label: "Accessibility",
+    id: "accessibility",
+  },
   {
     label: "First Timer Friendly",
     id: "first_timer_friendly",
-    color: tagColors.brown,
   },
   {
     label: "Tools",
     id: "tools",
-    color: tagColors.cyan,
   },
   {
     label: "Data Visualization",
     id: "data_viz",
-    color: tagColors.grassGreen,
   },
   {
     label: "Education",
     id: "education",
-    color: tagColors.hotPink,
   },
   {
     label: "Code Framework",
     id: "code_framework",
-    color: tagColors.hunterGreen,
   },
   {
     label: "Gaming",
     id: "gaming",
-    color: tagColors.lavender,
   },
   {
     label: "Starter Template",
     id: "starter_template",
-    color: tagColors.oceanBlue,
   },
   {
     label: "Productivity",
     id: "productivity",
-    color: tagColors.ochre,
   },
   {
     label: "Health",
     id: "health",
-    color: tagColors.purple,
   },
   {
     label: "Social Activism",
     id: "social_activism",
-    color: tagColors.rose,
   },
   {
     label: "Machine Learning",
     id: "machine_learning",
-    color: tagColors.rust,
   },
   {
     label: "Automation",
     id: "automation",
-    color: tagColors.scarlet,
   },
   {
     label: "Git",
     id: "git",
-    color: tagColors.seaGreen,
   },
   {
     label: "Environment",
     id: "environment",
-    color: tagColors.teal,
   },
   {
     label: "Browser Extension",
     id: "browser_extension",
-    color: tagColors.brown,
   },
   {
     label: "Scraping",
     id: "scraping",
-    color: tagColors.cyan,
   },
   {
     label: "Media",
     id: "media",
-    color: tagColors.goldYellow,
   },
   {
     label: "Android",
     id: "android",
-    color: tagColors.grassGreen,
   },
   {
     label: "E-Commerce",
     id: "e-commerce",
-    color: tagColors.hotPink,
   },
   {
     label: "DevOps",
     id: "devops",
-    color: tagColors.oceanBlue,
   },
   {
     label: "Infosec",
     id: "infosec",
-    color: tagColors.ochre,
   },
   {
     label: "CLI",
     id: "cli",
-    color: tagColors.purple,
   },
   {
     label: "Embedded Systems",
     id: "embedded_systems",
-    color: tagColors.rose,
   },
   {
     label: "Raspberry Pi",
     id: "raspberry-pi",
-    color: tagColors.rust,
   },
   {
     label: "Social",
     id: "social",
-    color: tagColors.scarlet,
   },
   {
     label: "iOS",
     id: "ios",
-    color: tagColors.seaGreen,
   },
   {
     label: "Web Application",
     id: "web_application",
-    color: tagColors.teal,
-  },
-  {
-    label: "Web Accessibility",
-    id: "accessibility",
-    color: tagColors.brown,
   },
   {
     label: "Cryptography",
     id: "cryptography",
-    color: tagColors.cyan,
   },
   {
     label: "Finance",
     id: "finance",
-    color: tagColors.goldYellow,
   },
 ];
 
