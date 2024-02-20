a2e7fda7422857bdf6f5e108811dc39b9cf3d50b
Maxime Preaux <deammer@users.noreply.github.com>
2024-02-20 08:36:20 -0800

    Show a shutdown banner (#351)

--- app/components/RootLayout.tsx
+++ app/components/RootLayout.tsx
@@ -11,6 +11,9 @@ const RootLayout: FC<Props> = ({ children, userInfo }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header userInfo={userInfo} />
+      <div className="p-2 text-center bg-white text-light-type border-b border-light-border">
+        Open Source Hub is shutting down on February 22, 2024.
+      </div>
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
